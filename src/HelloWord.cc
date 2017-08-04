ed92ba5a0d1c5639dcafbacbde7c397b01190047
mingthor <gaomingduke@gmail.com>
2017-08-04 09:35:39 -0700

    Add project pretty print, which serializes STL containers.

--- src/HelloWord.cc
+++ src/HelloWord.cc
@@ -1,5 +1,7 @@
 #include "../util/testharness.h"
+#include "../util/prettyprint.h"
 #include <iostream>
+#include <unordered_map>
 using namespace std;
 
 class HelloTest 
@@ -15,5 +17,10 @@ TEST(HelloTest, dummy)
 int main()
 {
     RunAllTests();
+
+    std::vector<int> foo({1,2,3}); 
+    std::cout << "My vector: " << foo << std::endl;
+    std::unordered_map<int, string> dict({{1,"a"},{2,"bc"},{3,"test"}});
+    std::cout<< "My map: " << dict <<std::endl;
     return 0;
 }
